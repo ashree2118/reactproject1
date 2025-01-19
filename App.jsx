89a03736d84dcd790e1acfe05ac54a16ab1d6a1a
Anushree <145786601+ashree2118@users.noreply.github.com>
2025-01-19 21:36:36 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/hero'
+import Analytics from './components/Analytics'
+import Newsletter from './components/Newsletter'
+import Cards from './components/Cards'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+    </>
+  )
+}
+
+export default App
